de2e4bdeace22ab2d0f98972cd84a1bcf95b077b
Yuqiangtan <56171017+Yuqiangtan@users.noreply.github.com>
2021-05-11 05:10:19 +0800

    Add files via upload

--- intro_to_express/index.js
+++ intro_to_express/index.js
@@ -1,43 +1,57 @@
-"use strict"
-// import {getAll, getItem,students} from './data.js';
-import * as data from "../data.js";
+'use strict'
+
 import express from 'express';
-import handlebars from "express-handlebars";
+import handlebars from "express-handlebars"
+import {Student} from "../models/data.js";
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.use(express.static('./public'));
-app.use(express.urlencoded());
-app.use(express.json());
+app.set("port", process.env.PORT || 3000);
+app.use(express.static('./public')); // allows direct navigation to static files
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(express.json()); //Used to parse JSON bodies
 
-app.engine('hbs', handlebars({defaultLayout:"main.hbs"}));
-app.set("view engine","hbs");
+app.engine('hbs', handlebars({defaultLayout: "main.hbs"}));
+app.set("view engine", "hbs");
 
 app.get('/', (req,res) => {
-    res.render('home', {data: data.getAll()});
+    Student.find({}).lean()
+        .then((students) => {
+            res.render('home', { data:students });
+        })
+        .catch(err => next(err));
 });
 
 app.get('/about', (req,res) => {
-    res.type('text/plain');
-    res.send('About page');
+    res.type('text/html');
+    res.render('about');
 });
 
-app.get('/detail', (req,res) => {
-    console.log(req.query)
-    let result = data.getItem(req.query.name);
-    res.render("details", {
-        name: req.query.name, 
-        result
-        }
-    );
+app.get('/detail', (req,res,next) => {
+    Student.findOne({ name:req.query.name }).lean()
+        .then((name) => {
+            res.render('details', {result: name} );
+        })
+        .catch(err => next(err));
 });
 
+app.post('/detail', (req,res, next) => {
+    Student.findOne({ name:req.body.name }).lean()
+        .then((name) => {
+            res.render('details', {result: name} );
+        })
+        .catch(err => next(err));
+});
 
-app.post('/detail', (req,res) => {
-    console.log(req.body)
-    let found = data.getItem(req.body.name);
-    res.render("details", {name: req.body.name, result: found, data: data.getAll()});
+app.get('/delete', (req,res) => {
+    Student.remove({ name:req.query.name }, (err, result) => {
+        if (err) return next(err);
+         let deleted = result.result.n !== 0;  // n will be 0 if no docs deleted
+        Student.count((err, total) => {
+             res.type('text/html'); 
+            res.render('delete', {name: req.query.name, deleted: result.result.n !== 0, total: total } );    
+        });
+    });
 });
 
 app.use((req,res) => {
